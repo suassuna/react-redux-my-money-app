9c4dbc1630d09fa33a9e28cef20c3e728d2860b2
suassuna <suassuna.paulo@gmail.com>
2018-05-29 12:24:11 -0300

    Added app footer.

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2018
+            <a href='http://kodohub.com' target='_blank'> kodohub</a>.
+        </strong>
+    </footer>
+)
