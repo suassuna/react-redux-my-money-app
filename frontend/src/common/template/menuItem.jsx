a19d8533172a61b5eae6beb10572b4a3b920651a
suassuna <suassuna.paulo@gmail.com>
2018-06-04 17:53:07 -0300

    Fixed menu item text is being cut whenever the menu collapse.

--- frontend/src/common/template/menuItem.jsx
+++ frontend/src/common/template/menuItem.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 export default props => (
     <li>
         <a href={props.path}>
-            <i className={`fa fa-${props.icon}`}></i> {props.label}
+            <i className={`fa fa-${props.icon}`}></i> <span>{props.label}</span>
         </a>
     </li>
 )
