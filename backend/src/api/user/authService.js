c0f6375a707615ad26fc34b3c62b2cc88a149cb4
suassuna <suassuna.paulo@gmail.com>
2018-06-05 00:58:39 -0300

    Added signup endpoint.

--- backend/src/api/user/authService.js
+++ backend/src/api/user/authService.js
@@ -22,7 +22,7 @@ const login = (req, res, next) => {
             return sendErrorsFromDB(res, err)
         } else if (user && bcrypt.compareSync(password, user.password)) {
             const token = jwt.sign(user, env.authSecret, {
-                expiresIn: "1 day"
+                expiresIn: '1 day'
             })
             const { name, email } = user
             res.json({ name, email, token })
@@ -34,8 +34,53 @@ const login = (req, res, next) => {
 
 const validateToken = (req, res, next) => {
     const token = req.body.token || ''
-    
+
     jwt.verify(token, env.authSecret, function(err, decoded) {
         return res.status(200).send({ valid: !err })
     })
 }
+
+const signup = (req, res, next) => {
+    const name = req.body.name || ''
+    const email = req.body.email || ''
+    const password = req.body.password || ''
+    const confirmPassword = req.body.confirm_password || ''
+
+    if (!email.match(emailRegex)) {
+        return res.status(400).send({ errors: ['O e-mail informado está inválido.'] })
+    }
+
+    if (!password.match(passwordRegex)) {
+        return res.status(400).send({
+            errors: [
+                'Senha precisa ter: uma letra maiúscula, uma letra minúscula, um número, um caractere especial(@#$%) e tamanho entre 6-20.'
+            ]
+        })
+    }
+
+    const salt = bcrypt.genSaltSync()
+    const passwordHash = bcrypt.hashSync(password, salt)
+    
+    if (!bcrypt.compareSync(confirmPassword, passwordHash)) {
+        return res.status(400).send({ errors: ['Senhas não conferem.'] })
+    }
+
+    User.findOne({ email }, (err, user) => {
+        if (err) {
+            return sendErrorsFromDB(res, err)
+        } else if (user) {
+            return res.status(400).send({ errors: ['Usuário já cadastrado.'] })
+        } else {
+            const newUser = new User({ name, email, password: passwordHash })
+            newUser.save(err => {
+                if (err) {
+                    return sendErrorsFromDB(res, err)
+                } else {
+                    login(req, res, next)
+                }
+            })
+        }
+    })
+}
+
+module.exports = { login, signup, validateToken }
