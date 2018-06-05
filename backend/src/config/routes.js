8472549e601b9be9134ec58c537a4fdc9a36393b
suassuna <suassuna.paulo@gmail.com>
2018-06-05 01:26:01 -0300

    Separate former api into a protected api (/api) and an opened api (/oapi)

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,12 +1,28 @@
 const express = require('express')
+const auth = require('./auth')
 
 module.exports = function(server) {
 
-  // base URL
-  const router = express.Router()
-  server.use('/api', router)
+  /**
+   * Rotas protegidas por Token JWT
+   */
+  const protectedApi = express.Router()
+  server.use('/api', protectedApi)
 
-  // billingCycle routes
+  protectedApi.use(auth)
+  
   const BillingCycle = require('../api/billingCycle/billingCycleService')
-  BillingCycle.register(router, '/billingCycles')
+  BillingCycle.register(protectedApi, '/billingCycles')
+
+  /**
+   * Rotas abertas
+   */
+  const openApi = express.Router()
+  server.use('/oapi', openApi)
+
+  const AuthService = require('../api/user/authService')
+  openApi.post('/login', AuthService.login)
+  openApi.post('/signup', AuthService.signup)
+  openApi.post('/validateToken', AuthService.validateToken)
+
 }
