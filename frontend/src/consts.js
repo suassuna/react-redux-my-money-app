689cd2f4878c2ca5053810c69c703eb081813bfb
suassuna <suassuna.paulo@gmail.com>
2018-06-08 18:00:06 -0300

    Added heroku configurations.

--- frontend/src/consts.js
+++ frontend/src/consts.js
@@ -1,4 +1,5 @@
 export default {
+    // change urls to production ones whenever you're publishing it at heroku.
     API_URL: 'http://localhost:3003/api',
     OAPI_URL: 'http://localhost:3003/oapi',
 }
